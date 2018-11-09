2bc421e074c5085089626c3acc1053a7dd4c4e49
Radoslaw Osinski <rad.osn@outlook.com>
2018-11-09 17:01:32 +0100

    Add files via upload

---
+++ AADB2C/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AADB2C.Models;
+using Microsoft.AspNetCore.Authorization;
+using CommunityCertForT;
+using Microsoft.Extensions.Configuration;
+using CommunityCertForT.Helpers;
+
+namespace AADB2C.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IConfiguration _configuration;
+        private AppSettings AppSettings { get; set; }
+
+        public HomeController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+            AppSettings = _configuration.GetSection("AppSettings").Get<AppSettings>();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Contact()
+        {
+            AADGraph graph = new AADGraph(AppSettings);
+            string groupName = "Admins";
+            string groupId = AppSettings.AADGroups.FirstOrDefault(g =>
+                String.Compare(g.Name, groupName) == 0).Id;
+            bool isIngroup = await graph.IsUserInGroup(User.Claims, groupId);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
